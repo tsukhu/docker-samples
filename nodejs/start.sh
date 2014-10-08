d27e078c867036056868b5ca379392c58cf7a085
Tarun Sukhu <tsukhu71@gmail.com>
2014-10-08 10:53:03 +0530

    Update start.sh

--- nodejs/start.sh
+++ nodejs/start.sh
@@ -1,11 +1,11 @@
 cd /tmp
-
 # try to remove the repo if it already exists
+
 rm -rf docker-samples; true
 
-git clone git@github.com:tsukhu/docker-samples.git
+git clone https://github.com/tsukhu/docker-samples.git
 
-cd docker-samples/nodejs/helloword
+cd ./docker-samples/nodejs/helloworld
 
 npm install
 
